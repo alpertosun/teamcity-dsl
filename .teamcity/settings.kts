81baeef3b89264920b0d5bc34182ada545fa512a
alpertosun <alpertosun@outlook.com.tr>
2024-05-06 17:03:29 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,8 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.build
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
 version = "2021.1"
 
 project {
