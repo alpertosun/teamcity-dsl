c9d65f494e68fb5db2e3a82b6c22217e7061e357
alpertosun <alpertosun@outlook.com.tr>
2024-05-06 17:08:49 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,40 +1,58 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-
-version = "2021.1"
-
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+version = "2019.2"
 project {
-    name = "Example Project"
-    id = Id("ExampleProject")
-
-    vcsRoot {
-        id = Id("ExampleVcsRoot")
-        name = "Example VCS Root"
-        type = "git"
-        param("url", "https://github.com/example/my-repo.git")
-        param("branch", "refs/heads/master")
-    }
-
-    buildType {
-        id = Id("ExampleBuild")
-        name = "Example Build"
-        vcs {
-            root(Id("ExampleVcsRoot"))
-        }
-        steps {
-            script {
-                name = "Compile and Build"
-                scriptContent = """
-                #!/bin/bash
-                echo "Starting build..."
-                """
-            }
-        }
-        triggers {
-            vcs {
-                branchFilter = "+:<default>"
-            }
-        }
+    vcsRoot(Sources)
+    buildType(Build)
+    buildType(Test)
+    sequential {
+        buildType(Build)
+        buildType(Test, options = { runOnSameAgent = true })
     }
 }
+
+object Build : BuildType({
+    name = "Build"
+
+    artifactRules = DslContext.getParameter("artifactPath")
+
+    vcs {
+        root(Sources)
+    }
+
+    steps {
+        maven {
+            goals = "package"
+            runnerArgs = "-DskipTests"
+        }
+    }
+})
+
+object Test : BuildType({
+    name = "Test"
+    buildNumberPattern = "${Build.depParamRefs.buildNumber}"
+
+    vcs {
+        root(Sources)
+    }
+
+    steps {
+        maven {
+            goals = "test"
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = ""
+        }
+    }
+
+})
+
+object Sources : GitVcsRoot({
+    name = "Sources"
+    url = DslContext.getParameter("repoUrl")
+})
